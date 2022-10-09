294276d0b89fc129412b616696a0bd3f6313e0e0
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-09 14:45:06 +0500

    Add rendering video title and description

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -13,6 +13,10 @@ const VideoDetail = ({ video }) => {
     
     return ( 
       <div>
+        <div className="ui segment">
+          <h4 className="ui header">{video.snippet.title}</h4>
+          <p className="">{video.snippet.description}</p>
+        </div>
         {video.snippet.title}
       </div>
     )
