842a2b26bf7d8263242c116743715dbf15b11f44
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-09 07:52:09 +0500

    Add state, onInputChange method, setState

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,23 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  
+  state = { term: ''};
+
+  onInputChange = (e) => {
+    this.setState({ term: e.target.value })
+  }  
+
   render() {
     return (
       <div className="search-bar ui segment">
         <form className="ui form">
           <div className="field">
             <label htmlFor="">Video Search</label>
-            <input type="text" />
+            <input 
+              type="text" 
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
           </div>
         </form>
       </div>
