8edfc9ae35e2e43e418bc71fa71e617ab047d362
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-09 11:13:25 +0500

    Add youtube api

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
 import React from "react";
-// import ReactDOM from "react-dom";
 import SearchBar from "./SearchBar";
+import youtube from "../api/youtube";
 
 class App extends React.Component {
+  onTermSubmit = (term) => {
+    console.log(term);
+    youtube.get('./search', {
+      params: {
+        q: term
+      }
+    })
+  }
+
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onTermSubmit} />
       </div>
     )
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -11,7 +11,7 @@ class SearchBar extends React.Component {
     e.preventDefault();
 
     //callback from parent component
-    
+    this.props.onFormSubmit(this.state.term)
   }
 
   render() {
